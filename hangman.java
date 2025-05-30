bf6e9db410b04efb40a4d48f81f9f4873bbb0de4
amymchai <amy.chai26@ycdsbk12.ca>
2025-05-30 11:03:36 -0400

    started creating the txt files

--- hangman.java
+++ hangman.java
@@ -1,1 +1,34 @@
+import arc.*;
 
+public class hangman{
+	public static void main(String[]args){
+		Console con = new Console();
+		menu(con);
+	}
+	public static void menu(Console con){
+		int intChoice = 0;
+		
+		while (intChoice==0){
+			con.println("1. Play Game");
+			con.println("2. Leaderboard");
+			con.println("3. Add Theme");
+			con.println("4. Quit");
+			con.println("Your choice: ");
+			intChoice = con.readInt();
+			
+			if (intChoice==1){
+				game(con);
+			}else if (intChoice==2){
+				leaderboard(con);
+			}else if (intChoice==3){
+				theme(con);
+			}else if (intChoice==4){
+				con.println("Thanks for playing!");
+			}else{
+				con.println("Options are between 1-4. Try again.");
+			}
+		}
+		
+	}
+}
+			
