688d8c1b72ed944a488e610c82da3b398f9a85b9
amymchai <amy.chai26@ycdsbk12.ca>
2025-06-02 11:03:28 -0400

    More hangman main coding (overall structure)

--- hangman.java
+++ hangman.java
@@ -3,32 +3,99 @@ import arc.*;
 public class hangman{
 	public static void main(String[]args){
 		Console con = new Console();
-		menu(con);
-	}
-	public static void menu(Console con){
-		int intChoice = 0;
 		
-		while (intChoice==0){
+		int intMenu = 0;
+		
+		//MAIN MENU
+		while(intMenu!=4){
+			con.println("HANGMAN");
 			con.println("1. Play Game");
-			con.println("2. Leaderboard");
+			con.println("2. View Leaderboard");
 			con.println("3. Add Theme");
-			con.println("4. Quit");
-			con.println("Your choice: ");
-			intChoice = con.readInt();
+			con.println("4. Quit");	
+			con.println("Select an option");
+			intMenu = con.readInt();
 			
-			if (intChoice==1){
+			if (intMenu==1){
+				//Play game code will go here
+				//game logic here
 				
-			}else if (intChoice==2){
+				con.println("TEST: play game");
+				
+				//Get player name
+				String strPlayerName;
+				con.println("What is your name?");
+				strPlayerName = con.readLine();
+				
+				//Count themes
+				String strThemes[];
+				TextInputFile CountThemes = new TextInputFile("themes.txt");
+				String strTemp;
+				int intThemeCount = 0;
+				while(CountThemes.eof()==false){
+					strTemp = CountThemes.readLine();
+					intThemeCount++;
+				}
+				CountThemes.close();
+				//con.println("TEST:"+intThemeCount);
+				
+				//Load data into array
+				strThemes = new String[intThemeCount];
+				int intCount;
+				CountThemes = new TextInputFile("themes.txt");
+				for(intCount = 0; intCount < intThemeCount; intCount++){
+					strThemes[intCount]=CountThemes.readLine();
+				}
+				CountThemes.close();
+				
+				//Print themes
+				con.println("Here are the themes:");
+				for(intCount = 0; intCount < intThemeCount; intCount++){
+					con.println(strThemes[intCount]);
+				}
+				
+				//Choose theme
+				con.println("Enter your theme choice:");
+				String strThemeChoice = con.readLine();
+				
+				//Count words in theme file
+				TextInputFile WordCount = new TextInputFile(strThemeChoice);
+				int intWordCount = 0;
+				while(WordCount.eof()==false){
+					WordCount.readLine();
+					intWordCount++;
+				}
+				WordCount.close();
+				
+				//Words array
+				String strWords[][] = new String[intWordCount][2];
+				TextInputFile words = new TextInputFile(strThemeChoice);
+				
+				for(intCount= 0; intCount < intWordCount; intCount++){
+					strWords[intCount][1] = words.readLine();
+					strWords[intCount][1]=(int)(Math.random()*100);
+				
+				//Bubble sort
+				
+				
 
-			}else if (intChoice==3){
-				0
-			}else if (intChoice==4){
-				con.println("Thanks for playing!");
-			}else{
-				con.println("Options are between 1-4. Try again.");
+			
+					
+					
+				
+				
+				
+			}	
+				
+			}else if (intMenu==2){
+				//leaderboard code here
+				
+			}else if (intMenu==3){
+				//add theme code over here
 			}
 		}
-		
 	}
 }
-			
+
+	
+	
